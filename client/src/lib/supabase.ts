503016413ba9b9146549d57c9d685d5a22a86107
ignarru <arruvitoignacio@gmail.com>
2025-06-05 21:28:53 -0300

    Update supabase.ts

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL as string
-const supabaseKey = process.env.SUPABASE_ANON_KEY as string
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
