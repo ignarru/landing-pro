079e22b5b0ee86ff45b35d72318650ceb4675c9c
ignarru <arruvitoignacio@gmail.com>
2025-06-06 08:04:40 -0300

    Create DetailedBrain.tsx

---
+++ client/src/components/DetailedBrain.tsx
@@ -0,0 +1,6 @@
+import { BrainCog } from "lucide-react";
+import type { SVGProps } from "react";
+
+export default function DetailedBrain(props: SVGProps<SVGSVGElement>) {
+  return <BrainCog {...props} />;
+}
