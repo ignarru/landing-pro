51cfca7f115b6fe3f8f43b4c6631d952328b62b9
ignarru <arruvitoignacio@gmail.com>
2025-06-06 09:07:57 -0300

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -2,5 +2,5 @@ import { drizzle } from "drizzle-orm/neon-serverless";
 import { Pool } from "@neondatabase/serverless";
 import * as schema from "../shared/schema";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString: process.env.SUPABASE_URL });
 export const db = drizzle(pool, { schema });
