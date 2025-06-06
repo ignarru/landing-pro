a689bf52a5a5934de14faaa058f12206d8f0f117
ignarru <arruvitoignacio@gmail.com>
2025-06-06 18:20:19 -0300

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -5,8 +5,12 @@ import Services from "@/components/Services";
 import WorkProcess from "@/components/WorkProcess";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, []);
   return (
     <div className="min-h-screen">
       <Navigation />
