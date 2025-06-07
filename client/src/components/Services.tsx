762325714657d254cfbd8f16fc917a5fa0260dcd
ignarru <arruvitoignacio@gmail.com>
2025-06-07 17:50:25 -0300

    Update Services.tsx

--- client/src/components/Services.tsx
+++ client/src/components/Services.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react
+import { useState, useEffect, useRef } from "react";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { useInView } from "@/hooks/use-in-view";
 import { 
