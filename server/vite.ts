9731f1e9fb2c190d6006c88cde1ff1012c47fa66
ignarru <arruvitoignacio@gmail.com>
2025-06-07 14:35:15 -0300

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -78,7 +78,7 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath));
+  app.use(express.static(distPath, { maxAge: "1y", index: false }));
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
